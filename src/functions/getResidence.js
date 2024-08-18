9765687367efd6dae7fada6eccfc40a7fc8d9ef1
eyalyehiely <eyalwork0@gmail.com>
2024-08-18 19:21:16 +0300

    add api to residence

--- src/functions/getResidence.js
+++ src/functions/getResidence.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-export default async function getAddress(setAddressResults) {
+export default async function getResidence(setResidenceResults) {
   try {
     const response = await axios.get('https://restcountries.com/v3.1/all/');
-    setAddressResults(response.data.result.name.official);
+    const countryNames = response.data.map(country => country.name.official);
+    setResidenceResults(countryNames);
   } catch (error) {
-    console.error('Error fetching address data:', error);
-    setAddressResults([]); // Set empty array or handle error state as needed
+    console.error('Error fetching residence data:', error);
+    setResidenceResults([]); // Set empty array or handle error state as needed
   }
 }
