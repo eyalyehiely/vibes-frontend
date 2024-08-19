d3022c05c1a62d798fd69f8da8bf4ab77a2cef21
eyalyehiely <eyalwork0@gmail.com>
2024-08-19 18:48:43 +0300

    add checkTalentTOken

---
+++ src/functions/talents/checkTalentToken.js
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode';
+
+const checkTalentToken = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')).access : null;
+
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+
+        // Check if the token is expired
+        if (decodedToken.exp < currentTime) {
+          localStorage.removeItem('authTokens');
+          navigate('/signin');
+        }
+
+        // Check if the license type is not 'Talent'
+        if (decodedToken.license_type !== 'Talent') {
+          navigate(-1);  // Navigate back to the previous page
+        }
+      } catch (error) {
+        console.error('Invalid token:', error);
+        localStorage.removeItem('authTokens');
+        navigate('/signin');
+      }
+    } else {
+      navigate('/signin');
+    }
+  }, [navigate]);  // Only depends on `navigate`, runs on mount and route changes
+};
+
+export default checkTalentToken;
