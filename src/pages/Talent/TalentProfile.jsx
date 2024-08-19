2143b0163b3006fa0781a077f2e550959b43feac
eyalyehiely <eyalwork0@gmail.com>
2024-08-19 18:49:27 +0300

    update talent profile component

--- src/pages/Talent/TalentProfile.jsx
+++ src/pages/Talent/TalentProfile.jsx
@@ -2,91 +2,18 @@ import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 import React, { useState, useEffect } from "react";
 import userThree from "../../images/user/user-03.png";
 import DefaultLayout from "../../layout/DefaultLayout";
-import getResidence from "../../functions/getResidence";
-import getLanguages from "../../functions/getLanguages";
-import saveTalentInfo from "../../functions/talents/saveTalentInfo";
+import EditTalentProfile from "./EditTalentProfile";
 import getTalentDetails from "../../functions/talents/getTalentDetails";
-import EditTalentProfile from './EditTalentProfile'
+import checkTalentToken from '../../functions/talents/checkTalentToken';
 
 const TalentProfile = () => {
+  // checkTalentToken()
   const [talent, setTalent] = useState({});
-  const [filteredResidence, setFilteredResidence] = useState([]);
-  const [residenceSearchQuery, setResidenceSearchQuery] = useState("");
-  const [residenceResults, setResidenceResults] = useState([]);
-  const [languagesResult, setLanguagesResult] = useState([]);
-  const [filteredlanguages, setFilteredlanguages] = useState([]);
-  const [languagesSearchQuery, setLanguagesSearchQuery] = useState("");
-  const [profileFormData, setprofileFormData] = useState({
-    first_name: "",
-    last_name: "",
-    gender: "male",
-    email: "",
-    phone_number: "",
-    residence: "",
-    languages: [],
-    job_type: "office",
-    job_sitting: "",
-    field_of_interest: [],
-    social_links: "",
-    skills: [],
-    work_history: [],
-    companies_black_list: [],
-    about_me: "",
-    is_open_to_work: false,
-    cv: "",
-    recommendation_letter: "",
-  });
-
   // useEffect(() => {
   //   if (token) {
   //     getTalentDetails(token, setTalent);
   //   }
   // }, [token]);
-
-  // residence
-  useEffect(() => {
-    getResidence(setResidenceResults);
-  }, []);
-
-  useEffect(() => {
-    if (residenceSearchQuery) {
-      const query = residenceSearchQuery.toLowerCase();
-      setFilteredResidence(
-        residenceResults.filter((residence) =>
-          residence.toLowerCase().includes(query)
-        )
-      );
-    } else {
-      setFilteredResidence(residenceResults);
-    }
-  }, [residenceSearchQuery, residenceResults]);
-
-  const handleResidenceSearchChange = (e) => {
-    setResidenceSearchQuery(e.target.value);
-  };
-
-  // languages
-
-  useEffect(() => {
-    getLanguages(setLanguagesResult);
-  }, []);
-
-  useEffect(() => {
-    if (languagesSearchQuery) {
-      const query = languagesSearchQuery.toLowerCase();
-      setFilteredlanguages(
-        languagesResult.filter((languages) =>
-          languages.toLowerCase().includes(query)
-        )
-      );
-    } else {
-      setFilteredlanguages(languagesResult);
-    }
-  }, [languagesSearchQuery, languagesResult]);
-
-  const handleLanguagesSearchChange = (e) => {
-    setLanguagesSearchQuery(e.target.value);
-  };
 
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -100,34 +27,6 @@ const TalentProfile = () => {
     e.preventDefault();
     // handle form submission logic here
   };
-
-  const {
-    first_name,
-    last_name,
-    phone_number,
-    email,
-    gender,
-    residence,
-    profile_pic,
-    job_type,
-    job_sitting,
-    field_of_interest,
-    social_links,
-    skills,
-    languages,
-    work_history,
-    companies_black_list,
-    about_me,
-    is_open_to_work,
-    cv,
-    recommendation_letter,
-  } = profileFormData;
-
-  const handlePersonalInfoSubmit = (e) => {
-    e.preventDefault();
-    saveTalentInfo(profileFormData);
-  };
-
   return (
     <DefaultLayout>
       <div className="mx-auto max-w-270">
@@ -139,7 +38,6 @@ const TalentProfile = () => {
               <div className="border-b border-stroke px-7 py-4 dark:border-strokedark">
                 <h3 className="font-medium text-black dark:text-white">
                   Talent Information
-                  <EditTalentProfile/>
                 </h3>
               </div>
               <div className="p-7">
@@ -208,21 +106,6 @@ const TalentProfile = () => {
                       >
                         Residence: {talent.residence}
                       </label>
-                      {/* <span
-                      className="w-full rounded border border-stroke bg-gray px-4.5 py-3 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                      type="text"
-                      name="residence"
-                      id="residence"
-                      list="residenceList"
-                      value={talent.residence}
-                      onChange={handleChange}
-                      required
-                    />
-                    <datalist id="residenceList">
-                      {filteredResidence.map((residence, index) => (
-                        <option key={index} value={residence} />
-                      ))}
-                    </datalist> */}
                     </div>
                   </div>
 
@@ -290,21 +173,6 @@ const TalentProfile = () => {
                       >
                         Languages:
                       </label>
-                      {/* <span
-                        className="w-full rounded border border-stroke bg-gray px-4.5 py-3 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                        type="text"
-                        name="languages"
-                        id="languages"
-                        list="languagesList"
-                        value={talent.languages}
-                        onChange={handleChange}
-                        required
-                      />
-                      <datalist id="languagesList">
-                        {filteredlanguages.map((languages, index) => (
-                          <option key={index} value={languages} />
-                        ))}
-                      </datalist> */}
                     </div>
                   </div>
 
@@ -351,7 +219,7 @@ const TalentProfile = () => {
                       className="mb-3 block text-sm font-medium text-black dark:text-white"
                       htmlFor="about me"
                     >
-                      Are you open to work ?
+                      Open to work:
                       <span
                         type="checkbox"
                         name="isOpenToWork"
@@ -363,12 +231,7 @@ const TalentProfile = () => {
                   </div>
 
                   <div className="flex justify-end gap-4.5">
-                    <button
-                      className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
-                      type="submit"
-                    >
-                      Save
-                    </button>
+                    <EditTalentProfile />
                   </div>
                 </form>
               </div>
