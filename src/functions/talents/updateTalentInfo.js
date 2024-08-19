192e36cde0a66941b81e01f26b32f0561af28dbe
eyalyehiely <eyalwork0@gmail.com>
2024-08-20 00:50:04 +0300

    update update talentInfo func

--- src/functions/talents/updateTalentInfo.js
+++ src/functions/talents/updateTalentInfo.js
@@ -1,35 +1,34 @@
-import axios from '../config/axiosConfig'
+import axios from '../config/axiosConfig';
 import getTalentDetails from './getTalentDetails';
+import swal from 'sweetalert'; 
 
-export default function updateTalentInfo(setTalent,data,handleClose,talent_id) {
-  axios.put(`/auth/edit_user/${talent_id}`, data, {
+export default function updateTalentInfo(setTalent, data, handleClose, talent_id, token) {
+  axios.put(`/users/talent/${talent_id}/`, data, {
     headers: {
       'content-type': 'application/json',
-      // Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     }
   })
     .then(response => {
-      if (response.data.status === 200) {
+      if (response.status === 200) {
         swal({
           title: 'User updated successfully!',
           icon: 'success',
-          timer:1000,
+          timer: 1000,
           button: false,
-        }).then(()=>{
-          getTalentDetails(token, setTalent,talent_id);
-          window.location.reload()
+        }).then(() => {
+          getTalentDetails(token, setTalent, talent_id);
           handleClose();
-        })
-       
+        });
       } else {
         console.log('Error:', response.data.message);
-
       }
     })
     .catch(error => {
-      console.error('שגיאה!', error);
+      console.error('Error!', error);
       swal({
         title: 'Error!',
+        text: error.response?.data?.message || 'An error occurred during the update process.',
         icon: 'warning',
         button: 'OK',
       });
