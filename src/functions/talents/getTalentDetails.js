f2f83441c841e2fc4e573987a0a0db15c7822729
eyalyehiely <eyalwork0@gmail.com>
2024-08-20 00:49:49 +0300

    update get talent details

--- src/functions/talents/getTalentDetails.js
+++ src/functions/talents/getTalentDetails.js
@@ -1,21 +1,20 @@
-
 import axios from '../config/axiosConfig';
 
-export default async function getTalentDetails(setTalent,talent_id) {
-
-    await axios.get(`/usres/talent/${talent_id}`, {}, {
+export default async function getTalentDetails(token, setTalent, talent_id) {
+  try {
+    const response = await axios.get(`/users/talent/${talent_id}/`, {
       headers: {
         'Content-Type': 'application/json',
-        // Authorization: `Bearer ${token}`,
-      }
-    }).then(response => {
-      if (response.data.status === 200) {
-        setTalent(response.data);
-        
-      } else {
-        console.log('Error:', response.data.message);
-      }
-    }).catch(error => {
-      console.error('There was an error in fetching current user data!', error);
+        Authorization: `Bearer ${token}`,
+      },
     });
+
+    if (response.status === 200) {
+      setTalent(response.data);
+    } else {
+      console.log('Error:', response.data.message);
+    }
+  } catch (error) {
+    console.error('There was an error in fetching current user data!', error);
   }
+}
