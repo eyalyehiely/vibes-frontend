599079ccfe8110979ab314746f60b11220789196
eyalyehiely <eyalwork0@gmail.com>
2024-09-01 01:11:08 +0300

    add available jobs to the side bar

--- src/Companies/components/CompanySidebar/CompanySideBar.tsx
+++ src/Companies/components/CompanySidebar/CompanySideBar.tsx
@@ -400,16 +400,13 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                         <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 pl-6">
                           <li>
                             <NavLink
-                              to="/tasks/task-list"
+                              to="/company/Available-Jobs"
                               className={({ isActive }) =>
                                 'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
                                 (isActive && '!text-white')
                               }
                             >
-                              List
-                              <span className="absolute right-4 block rounded bg-primary py-1 px-2 text-xs font-medium text-white">
-                                Pro
-                              </span>
+                              Available Jobs
                             </NavLink>
                           </li>
                           <li>
