7db37657c2af020e45d23d26e9b28bea74a469b6
eyalyehiely <eyalwork0@gmail.com>
2024-09-01 19:09:14 +0300

    add other response to resetPASSWORD FUNC

--- src/generalFunctions/auth/sendResetPasswordLink.js
+++ src/generalFunctions/auth/sendResetPasswordLink.js
@@ -2,38 +2,35 @@ import axios from '../../generalFunctions/config/axiosConfig';
 import swal from 'sweetalert';
 
 export default function fetchData(event) {
-  event.preventDefault();
+  // event.preventDefault();
 
   const email = document.getElementById('email').value;
   axios.post('/users/auth/request-password-reset/', { email })
     .then((response) => {
       if (response.status === 200) {
         swal({
-          title: "קישור נשלח בהצלחה",
+          title: "Link has been sent successfully",
           icon: "success",
           timer: 1000,
           button: false,
         }).then(() => {
-          window.location.href = '/signin';
+          window.location.href = '/auth/signin';
         });
       } else {
+        (response.status === 404)
         swal({
-          title: "משתמש לא נמצא",
+          title: "User not found",
           icon: "warning",
-          button: "אישור",
+          button: "OK",
         });
       }
     })
     .catch((error) => {
       console.error('Error occurred:', error);
       swal({
-        title: "שגיאה",
-        text: "משהו השתבש, אנא נסה שוב",
+        title: "Error",
         icon: "warning",
-        button: "אישור",
-      })
-      .then(()=>{
-        window.location.href = '/signin';
+        button: "OK",
       })
     });
 }
