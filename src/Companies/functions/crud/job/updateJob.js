af5baf8ac55c915c17e628090b5712f0061677d2
eyalyehiely <eyalwork0@gmail.com>
2024-09-03 20:13:05 +0300

    improve updateJob function

--- src/Companies/functions/crud/job/updateJob.js
+++ src/Companies/functions/crud/job/updateJob.js
@@ -1,21 +1,36 @@
 import axios from '../../../../generalFunctions/config/axiosConfig';
 import swal from 'sweetalert';
 
-const updateJob = async (job_id,company_id, jobData, token) => {
-  try {
-    const response = await axios.put(`users/company/${company_id}/job/${job_id}`, jobData, {
+export default async function updateJob(job_id, jobData, token,handleClose, setJobs){
+    await axios.put(`users/company/job/${job_id}`, jobData, {
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
-    });
-    swal('Success!', 'Job updated successfully!', 'success');
-    return response.data;
-  } catch (error) {
-    console.error('Error updating job:', error.response?.data || error.message);
-    swal('Error!', error.response?.data?.error || 'An error occurred while updating the job.', 'error');
-    throw error;
-  }
-};
+    }).then(response => {
+      if (response.status === 200) {
+        swal({
+          title: 'Job updated successfully!',
+          icon: 'success',
+          timer: 1000,
+          button: false,
+        }).then(()=>{
+          getJobDetails(job_id, company_id, token, setJobs)
+          handleClose()
+        })
+      } else {
+        console.log('Error:', response.data.message);
+      }
+    })
 
-export default updateJob;
+      .catch(error => {
+        console.error('Error!', error);
+        swal({
+          title: 'Error!',
+          text: error.response?.data?.message || 'An error occurred during the update process.',
+          icon: 'warning',
+          button: 'OK',
+        });
+      });
+    }
+  
