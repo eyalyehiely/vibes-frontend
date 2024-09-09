56f46f4d917e527a1aac71f7adbfeae1a65a2922
eyalyehiely <eyalwork0@gmail.com>
2024-09-09 20:19:26 +0300

    add talent table for available jobs

---
+++ src/Companies/components/Jobs/TalentsTable.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+ // Adjust the path based on your folder structure
+
+function TalentsTable({ job_id }) {
+
+  return (
+    <div>gfdgd</div>
+  );
+}
+
+export default TalentsTable;
