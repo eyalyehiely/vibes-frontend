f2c30faffb74a68921077d48f328e51d2f7a8bad
eyalyehiely <eyalwork0@gmail.com>
2024-09-16 00:35:36 +0700

    update edit job & update

--- src/Companies/functions/crud/job/updateJob.js
+++ src/Companies/functions/crud/job/updateJob.js
@@ -1,36 +1,39 @@
 import axios from '../../../../generalFunctions/config/axiosConfig';
 import swal from 'sweetalert';
+import getJobDetails from '../job/getJobDetails'; 
 
-export default async function updateJob(job_id, jobData, token,handleClose, setJobs){
-    await axios.put(`users/company/job/${job_id}`, jobData, {
+export default async function updateJob(job_id, jobData, token, handleClose, setJobs) {
+  try {
+    const response = await axios.put(`users/company/job/${job_id}/`, jobData, {
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
-    }).then(response => {
-      if (response.status === 200) {
-        swal({
-          title: 'Job updated successfully!',
-          icon: 'success',
-          timer: 1000,
-          button: false,
-        }).then(()=>{
-          getJobDetails(job_id, company_id, token, setJobs)
-          handleClose()
-        })
-      } else {
-        console.log('Error:', response.data.message);
-      }
-    })
+    });
 
-      .catch(error => {
-        console.error('Error!', error);
-        swal({
-          title: 'Error!',
-          text: error.response?.data?.message || 'An error occurred during the update process.',
-          icon: 'warning',
-          button: 'OK',
-        });
+    if (response.status === 200) {
+      swal({
+        title: 'Job updated successfully!',
+        icon: 'success',
+        timer: 1000,
+        button: false,
+      }).then(() => {
+        handleClose(); // Close the modal after the success alert
+        getJobDetails(job_id, token, setJobs);
       });
+    } else {
+      console.log('Error:', response.data.message);
     }
+  } catch (error) {
+    console.error('Error!', error);
+    swal({
+      title: 'Error!',
+      text: error.response?.data?.message || 'An error occurred during the update process.',
+      icon: 'warning',
+      button: 'OK',
+    });
+  }
+}
+
+
   
