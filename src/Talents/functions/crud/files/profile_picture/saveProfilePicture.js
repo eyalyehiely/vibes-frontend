7a0cf48791bc96fba7b4e598c5c5f09f03c15ba4
eyalyehiely <eyalwork0@gmail.com>
2024-10-04 18:17:36 +0700

    add save profile pic function

---
+++ src/Talents/functions/crud/files/profile_picture/saveProfilePicture.js
@@ -0,0 +1,38 @@
+import axios from '../../../../../generalFunctions/config/axiosConfig'
+import swal from 'sweetalert';
+import getTalentDetails from "../../../../../Recruiters/functions/crud/getRecruiterDetails";
+
+export default async function saveProfilePicture(profilePic, token, talent_id, setTalent) {
+  if (profilePic && profilePic.file) {
+    const formData = new FormData();
+    formData.append('profile_picture', profilePic.file);
+
+    try {
+      const response = await axios.post(`/users/manage-profile-pic/${talent_id}/`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        swal({
+          title: 'profile picture saved successfully!',
+          icon: 'success',
+          timer: 1000,
+          button: false,
+        });
+        // Re-fetch the talent details after saving the profile picture
+        getTalentDetails(token, setTalent, talent_id);
+      }
+    } catch (error) {
+      console.error('Error saving profile picture:', error);
+      swal({
+        title: 'Error!',
+        text: error.message,
+        icon: 'error',
+        button: 'OK',
+      });
+    }
+  }
+}
