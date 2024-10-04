692959fa5b375c8fbcb859c39972395de65d6c91
eyalyehiely <eyalwork0@gmail.com>
2024-10-04 18:17:17 +0700

    add delete profile pic function

---
+++ src/Talents/functions/crud/files/profile_picture/deleteProfilePicture.js
@@ -0,0 +1,41 @@
+import axios from '../../../../../generalFunctions/config/axiosConfig'
+import swal from 'sweetalert';
+
+export default async function deleteProfilePicture(setProfilePicture, token, talent_id, setTalent) {
+  swal({
+    title: 'Are you sure?',
+    text: 'Once deleted, you will not be able to recover this ProfilePicture!',
+    icon: 'warning',
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      axios.delete(`/users/manage-profile-pic/${talent_id}/`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`, // Ensure correct formatting
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setTalent(response.data)
+          setProfilePicture(null);
+          swal('Profile picture deleted successfully!', {
+            icon: 'success',
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error deleting Profile picture:', error);
+        swal({
+          title: 'Error!',
+          text: 'Failed to delete Profile picture.',
+          icon: 'error',
+          button: 'OK',
+        });
+      });
+    } else {
+      swal('Your Profile picture is safe!');
+    }
+  });
+};
