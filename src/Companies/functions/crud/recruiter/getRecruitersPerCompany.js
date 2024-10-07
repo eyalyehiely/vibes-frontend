df6361a4ea2802b7a8529a448de3c9b2ea8b2fd9
eyalyehiely <eyalwork0@gmail.com>
2024-10-07 11:04:23 +0700

    update get recruiter company handling

--- src/Companies/functions/crud/recruiter/getRecruitersPerCompany.js
+++ src/Companies/functions/crud/recruiter/getRecruitersPerCompany.js
@@ -5,16 +5,18 @@ export default async function getRecruitersPerCompany(token, setRecruiters, comp
     const response = await axios.get(`/users/company/${company_id}/recruiters/`, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     });
 
-    if (response.status === 200) {
+    if (response.status === 200 && response.data) {
       setRecruiters(response.data);
     } else {
-      console.log('Error:', response.data.message);
+      console.log('Unexpected response:', response);
+      setRecruiters([]); // Set to an empty array if no data is returned
     }
   } catch (error) {
-    console.error('There was an error in fetching recruiters data!', error);
+    console.error('There was an error in fetching recruiters data!', error.message || error);
+    setRecruiters([]); // Reset recruiters to an empty array in case of an error
   }
 }
