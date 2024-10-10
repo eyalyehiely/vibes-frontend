df8168bbfa6d0a685113d98df6a11d05152e87a8
eyalyehiely <eyalwork0@gmail.com>
2024-10-10 12:31:35 +0700

    create CompanyTalentsPage

---
+++ src/Companies/pages/CompanyTalentsPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import SearchTalents from "../components/Jobs/SearchTalents"; 
+import CompanyDefaultLayout from "../components/CompanyDefaultLayout";
+import checkCompanyToken from "../functions/auth/checkCompanyToken";
+
+
+function CompanyTalentsPage() {
+  checkCompanyToken();
+  const { job_id } = useParams(); // Retrieve job_id from URL
+
+  return (
+    <CompanyDefaultLayout>
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Talents for Job ID: {job_id} </h2>
+      <SearchTalents job_id={job_id} />
+    </div>
+    </CompanyDefaultLayout>
+  );
+}
+
+export default CompanyTalentsPage;
