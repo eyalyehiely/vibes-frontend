3f78103cd1fd0e38585d27ac7cf55a164de65072
eyalyehiely <eyalwork0@gmail.com>
2024-10-15 18:17:16 +0700

    change talent contact us submit button

--- src/Talents/pages/TalentContactUs.jsx
+++ src/Talents/pages/TalentContactUs.jsx
@@ -179,9 +179,9 @@ const TalentContactUs = () => {
 
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
+                  className="flex w-full justify-center rounded bg-purple-500 p-3 font-medium text-white hover:bg-purple-600"
                 >
-                  Send Message
+                  Send Us Message !
                 </button>
               </div>
             </form>
