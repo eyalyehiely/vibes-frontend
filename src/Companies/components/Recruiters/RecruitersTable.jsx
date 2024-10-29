f07c10b13ea5f53d87698e70f5234de2d26a02df
eyalyehiely <eyalwork0@gmail.com>
2024-10-29 12:16:50 +0700

    comment button

--- src/Companies/components/Recruiters/RecruitersTable.jsx
+++ src/Companies/components/Recruiters/RecruitersTable.jsx
@@ -306,6 +306,19 @@ function RecruitersTable() {
                           <path d="M19.7 8.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM12.6 22H10v-2.6l6-6 2.6 2.6-6 6zm7.4-7.4L17.4 12l1.6-1.6 2.6 2.6-1.6 1.6z" />
                         </svg>
                       </button>
+
+                      {/* <button
+    className="ml-4 rounded-full text-blue-500 hover:text-blue-600"
+    onClick={() => handleEditClick(recruiter)}  // Trigger modal for deeper editing
+  >
+    <span className="sr-only">Edit in Modal</span>
+    <svg
+      className="h-8 w-8 fill-current"
+      viewBox="0 0 32 32"
+    >
+      <path d="M19.7 8.3c-.4-.4-1-.4-1.4 0l-10 10c-.2.2-.3.4-.3.7v4c0 .6.4 1 1 1h4c.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4l-4-4zM12.6 22H10v-2.6l6-6 2.6 2.6-6 6zm7.4-7.4L17.4 12l1.6-1.6 2.6 2.6-1.6 1.6z" />
+    </svg>
+  </button> */}
                       <button
                         className="ml-4 rounded-full text-rose-500 hover:text-rose-600"
                         onClick={() =>
