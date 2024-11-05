bddb8ba16697065438d88e285fd4b0ffd91d877d
eyalyehiely <eyalwork0@gmail.com>
2024-11-06 00:42:01 +0700

    update create job function

--- src/Companies/functions/crud/job/createJob.js
+++ src/Companies/functions/crud/job/createJob.js
@@ -1,15 +1,16 @@
 import axios from '../../../../generalFunctions/config/axiosConfig';
-import swal from 'sweetalert'; 
-import getCompanyJobs from './getCompanyJobs'
+import swal from 'sweetalert';
+import getCompanyJobs from './getCompanyJobs';
 
-export default function createJob(data, company_id, token, setJobs, handleClose) {
-  axios.post(`users/company/${company_id}/job/`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    }
-  })
-    .then(response => {
+export default function createJob(data, company_id, token, setJobs = () => {}, handleClose) {
+  axios
+    .post(`users/company/${company_id}/job/`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
       if (response.status === 201) {
         swal({
           title: 'Job created successfully!',
@@ -18,15 +19,15 @@ export default function createJob(data, company_id, token, setJobs, handleClose)
           button: false,
         }).then(() => {
           // Add the new job to the current list of jobs
-          setJobs(prevJobs => [...prevJobs, response.data]);
-          getCompanyJobs(company_id, token, setJobs)
+          setJobs((prevJobs) => [...prevJobs, response.data]);
+          getCompanyJobs(company_id, token, setJobs);
           handleClose(); // Close the modal
         });
       } else {
         console.log('Error:', response.data.message);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.error('Error creating job:', error.response?.data || error.message);
       swal({
         title: 'Error!',
