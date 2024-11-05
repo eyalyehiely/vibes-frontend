9b2e993298d1adba4b0892e78b24aa50b06f4981
eyalyehiely <eyalwork0@gmail.com>
2024-11-06 01:06:43 +0700

    update create job function

--- src/Companies/functions/crud/job/createJob.js
+++ src/Companies/functions/crud/job/createJob.js
@@ -1,39 +1,38 @@
+
 import axios from '../../../../generalFunctions/config/axiosConfig';
 import swal from 'sweetalert';
-import getCompanyJobs from './getCompanyJobs';
-
-export default function createJob(data, company_id, token, setJobs = () => {}, handleClose) {
-  axios
-    .post(`users/company/${company_id}/job/`, data, {
+export default async function createJob(data, company_id, token, setJobs = () => {}, handleClose) {
+  try {
+    const response = await axios.post(`users/company/${company_id}/job/`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
-    })
-    .then((response) => {
-      if (response.status === 201) {
-        swal({
-          title: 'Job created successfully!',
-          icon: 'success',
-          timer: 1000,
-          button: false,
-        }).then(() => {
-          // Add the new job to the current list of jobs
-          setJobs((prevJobs) => [...prevJobs, response.data]);
-          getCompanyJobs(company_id, token, setJobs);
-          handleClose(); // Close the modal
-        });
-      } else {
-        console.log('Error:', response.data.message);
-      }
-    })
-    .catch((error) => {
-      console.error('Error creating job:', error.response?.data || error.message);
-      swal({
-        title: 'Error!',
-        text: error.response?.data?.error || 'An error occurred while creating the job.',
-        icon: 'warning',
-        button: 'OK',
-      });
     });
+
+    if (response.status === 201) {
+      handleClose(); // Close the modal first
+      swal({
+        title: 'Job created successfully!',
+        icon: 'success',
+        timer: 1000,
+        button: false,
+      });
+
+      setJobs((prevJobs) => [...prevJobs, response.data]); // Add the new job to the list
+      return response.data; // Return the created job
+    } else {
+      console.log('Error:', response.data.message);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error creating job:', error.response?.data || error.message);
+    swal({
+      title: 'Error!',
+      text: error.response?.data?.error || 'An error occurred while creating the job.',
+      icon: 'warning',
+      button: 'OK',
+    });
+    return null;
+  }
 }
