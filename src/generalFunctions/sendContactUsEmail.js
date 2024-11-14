3936a80d2795bfe23b76c5e7fb3980e832374f46
eyalyehiely <eyalwork0@gmail.com>
2024-11-14 17:59:22 +0700

    update send contact us email function

--- src/generalFunctions/sendContactUsEmail.js
+++ src/generalFunctions/sendContactUsEmail.js
@@ -1,43 +1,45 @@
-import axios from './config/axiosConfig';
+// import axios from './config/axiosConfig';
+import axios from 'axios'
 import swal from 'sweetalert';
 
 export default function sendContactUsEmail(formData, token) {
-  axios.post('/notifications/support/', formData, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-  })
-    .then(response => {
-      if (response.status === 200) {
-        swal({
-          title: 'Message sent successfully!',
-          icon: 'success',
-          timer: 1000,
-          button: false,
-        }).then(()=>{
-          window.location.reload();
-        })
-      } else {
-        const errorMessage = response.data.error || 'An unexpected error occurred';
-        console.log("Form data:", formData);
-        console.log('Error:', errorMessage);
-        swal({
-          title: 'Error',
-          text: errorMessage,
-          icon: 'error',
-          button: 'OK',
-        });
-      }
-    })
-    .catch(error => {
-      const errorMessage = error.response?.data?.error || 'An unexpected error occurred';
-      console.error('There was an error sending the email:', errorMessage);
+  console.log(token);
+  axios.post(`${import.meta.env.VITE_REACT_APP_NOTIFICATIONS_API_URL}/contact-us/`, formData, {
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+      },
+})
+  .then(response => {
+    if (response.status === 200) {
+      swal({
+        title: 'Message sent successfully!',
+        icon: 'success',
+        timer: 1000,
+        button: false,
+      }).then(() => {
+        window.location.reload();
+      });
+    } else {
+      const errorMessage = response.data.error || 'An unexpected error occurred';
+      console.log("Form data:", formData);
+      console.log('Error:', errorMessage);
       swal({
         title: 'Error',
         text: errorMessage,
         icon: 'error',
         button: 'OK',
       });
+    }
+  })
+  .catch(error => {
+    const errorDetails = error.response ? error.response.data : error.message;
+    console.error('Error response:', errorDetails);
+    swal({
+      title: 'Error',
+      text: errorDetails.detail || 'An unexpected error occurred',
+      icon: 'error',
+      button: 'OK',
     });
+  });
 }
