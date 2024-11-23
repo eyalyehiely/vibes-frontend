ae08b68add1a0f4b29d5817ac2752a87d7699be1
eyalyehiely <eyalwork0@gmail.com>
2024-11-23 13:23:47 +0700

    make cosmetics change

--- src/Recruiters/pages/Home/RecruiterPicHandling.jsx
+++ src/Recruiters/pages/Home/RecruiterPicHandling.jsx
@@ -10,14 +10,13 @@ import getRecruiterDetails from "../../functions/crud/getRecruiterDetails";
 import getCompanyDetails from "../../../Companies/functions/crud/company/getCompanyDetails";
 import checkRecruiterToken from "../../functions/auth/checkRecruiterToken";
 import { jwtDecode } from "jwt-decode";
-
-
+import { CiMenuKebab } from "react-icons/ci";
 
 function RecruiterPicHandling() {
   checkRecruiterToken();
   const [recruiter, setRecruiter] = useState({});
   const [company, setCompany] = useState({});
-  //   const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [profilePicture, setProfilePicture] = useState(null);
   const [cameraActive, setCameraActive] = useState(false);
   const [photoTaken, setPhotoTaken] = useState(null);
@@ -149,118 +148,109 @@ function RecruiterPicHandling() {
   const triggerProfilePictureUpload = () =>
     profilePictureInputRef.current.click();
   return (
-    <>
-    {/* <div className="col-span-3"> */}
-      {/* <div className="rounded-lg border border-stroke bg-white p-7 shadow-default dark:border-strokedark dark:bg-boxdark"> */}
-        {/* <h3 className="font-medium text-black dark:text-white">
-          Profile Picture
-        </h3> */}
-        <div className="p-0.5">
-          <div className="mt-3 flex justify-center">
-            {profilePicture ? (
-              <img
-                src={`${import.meta.env.VITE_BACKEND_API_BASE_URL}${
-                  recruiter.profile_picture
-                }`}
-                alt="User"
-                // className="mx-auto h-32 w-32 rounded-full object-cover"
-                className="h-26 w-26 rounded-full border-4 border-purple-500 object-cover shadow-lg"
-              />
-            ) : (
-              <>
-                <p>No Profile Picture</p>
-                <p className="text-gray-500">No Profile Picture</p>
-              </>
-            )}
-          </div>
-          <div className="mt-2 flex flex-row flex-wrap justify-center gap-2.5 sm:flex-row">
-            <button
-              className="mt-2 flex justify-center rounded bg-purple-500 px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
-              type="button"
-              onClick={startCamera}
-            >
-              <CiCamera size={19} className="mb-1" />
-            </button>
-            <video
-              ref={videoRef}
-              style={{
-                display: cameraActive ? "block" : "none",
-                width: "100%",
-              }}
-              className="max-w-xs"
-            ></video>
-            <canvas
-              ref={canvasRef}
-              style={{ display: "none" }}
-              width="640"
-              height="480"
-            ></canvas>
-            {cameraActive && (
-              <button
-                className="mt-2 flex justify-center rounded bg-purple-500 px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
-                type="button"
-                onClick={takePhoto}
-              >
-                <TbCapture size={19} className="mb-1" />
-              </button>
-            )}
+    
+    <div className="p-0.5">
+      <div className="mt-3 flex justify-center">
+        {profilePicture ? (
+          <img
+            src={`${import.meta.env.VITE_BACKEND_API_BASE_URL}${
+              recruiter.profile_picture
+            }`}
+            alt="User"
+            // className="mx-auto h-32 w-32 rounded-full object-cover"
+            className="h-26 w-26 rounded-full border-4 border-purple-500 object-cover shadow-lg"
+          />
+        ) : (
+          <>
+            <p>No Profile Picture</p>
+            <p className="text-gray-500">No Profile Picture</p>
+          </>
+        )}
+      </div>
+      
+      <div className="mt-2 flex flex-row flex-wrap justify-center gap-2.5 sm:flex-row">
+        <button
+          className="mt-2 flex justify-center rounded bg-purple-500 px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
+          type="button"
+          onClick={startCamera}
+        >
+          <CiCamera size={19} className="mb-1" />
+        </button>
+        <video
+          ref={videoRef}
+          style={{
+            display: cameraActive ? "block" : "none",
+            width: "100%",
+          }}
+          className="max-w-xs"
+        ></video>
+        <canvas
+          ref={canvasRef}
+          style={{ display: "none" }}
+          width="640"
+          height="480"
+        ></canvas>
+        {cameraActive && (
+          <button
+            className="mt-2 flex justify-center rounded bg-purple-500 px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
+            type="button"
+            onClick={takePhoto}
+          >
+            <TbCapture size={19} className="mb-1" />
+          </button>
+        )}
 
-            {cameraActive && (
-              <button
-                className="mt-2 flex justify-center rounded bg-yellow-500 px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
-                type="button"
-                onClick={closeCamera}
-              >
-                <IoMdClose size={19} className="mb-1" />
-              </button>
-            )}
-            {photoTaken && (
-              <div>
-                <h4 className="mt-2 text-sm font-medium">Captured Image:</h4>
-                <img
-                  src={photoTaken}
-                  alt="Captured"
-                  className="mt-2 h-24 w-24 rounded-full object-cover"
-                />
-              </div>
-            )}
-            <button
-              className="mt-2 flex justify-center rounded bg-primary px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
-              type="button"
-              onClick={triggerProfilePictureUpload}
-            >
-              <MdOutlineDriveFolderUpload size={19} className="mb-1" />
-            </button>
-            <input
-              type="file"
-              ref={profilePictureInputRef}
-              accept="image/*"
-              style={{ display: "none" }}
-              onChange={handleProfilePictureChange}
+        {cameraActive && (
+          <button
+            className="mt-2 flex justify-center rounded bg-yellow-500 px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
+            type="button"
+            onClick={closeCamera}
+          >
+            <IoMdClose size={19} className="mb-1" />
+          </button>
+        )}
+        {photoTaken && (
+          <div>
+            <h4 className="mt-2 text-sm font-medium">Captured Image:</h4>
+            <img
+              src={photoTaken}
+              alt="Captured"
+              className="mt-2 h-24 w-24 rounded-full object-cover"
             />
-
-            <button
-              className="mt-2 flex justify-center rounded bg-success px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
-              type="button"
-              onClick={handleProfilePictureUpload}
-            >
-              <CiBookmark size={19} className="mb-1" />
-            </button>
-            <button
-              className="mt-2 flex justify-center rounded bg-danger px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
-              type="button"
-              onClick={handleDeleteProfilePicture}
-            >
-              <IoTrashOutline size={19} className="mb-1" />
-            </button>
           </div>
-        </div>
-      {/* </div> */}
-    {/* </div> */}
-    </>
+        )}
+        <button
+          className="mt-2 flex justify-center rounded bg-primary px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
+          type="button"
+          onClick={triggerProfilePictureUpload}
+        >
+          <MdOutlineDriveFolderUpload size={19} className="mb-1" />
+        </button>
+        <input
+          type="file"
+          ref={profilePictureInputRef}
+          accept="image/*"
+          style={{ display: "none" }}
+          onChange={handleProfilePictureChange}
+        />
 
+        <button
+          className="mt-2 flex justify-center rounded bg-success px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
+          type="button"
+          onClick={handleProfilePictureUpload}
+        >
+          <CiBookmark size={19} className="mb-1" />
+        </button>
+        <button
+          className="mt-2 flex justify-center rounded bg-danger px-3 py-1.5 text-sm font-medium text-gray hover:bg-opacity-90"
+          type="button"
+          onClick={handleDeleteProfilePicture}
+        >
+          <IoTrashOutline size={19} className="mb-1" />
+        </button>
+      </div>
+    </div>
   );
-  
 }
 
 export default RecruiterPicHandling;
