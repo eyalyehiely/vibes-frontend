5da7534fbbce577248cd2e4c1b7dd8562e09875c
eyalyehiely <eyalwork0@gmail.com>
2024-11-24 12:34:31 +0700

    add recruoter calendar template

--- src/Recruiters/components/RecruiterSidebar/RecruiterSidebar.tsx
+++ src/Recruiters/components/RecruiterSidebar/RecruiterSidebar.tsx
@@ -127,7 +127,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Calendar --> */}
               <li>
                 <NavLink
-                  to="/calendar"
+                  to="/recruiter/calendar"
                   className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
                     pathname.includes('calendar') &&
                     'bg-graydark dark:bg-meta-4'
