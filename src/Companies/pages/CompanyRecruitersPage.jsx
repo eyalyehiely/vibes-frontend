48eb4eb99c23dbc1088d66d0c186410fed7ac3c3
eyalyehiely <eyalwork0@gmail.com>
2024-11-27 01:59:15 +0800

    romove checking tokens

--- src/Companies/pages/CompanyRecruitersPage.jsx
+++ src/Companies/pages/CompanyRecruitersPage.jsx
@@ -3,11 +3,9 @@ import Breadcrumb from "../../components/Breadcrumbs/Breadcrumb";
 // import TableFive from "../../components/Tables/TableFive";
 import RecruitersTable from "../components/Recruiters/RecruitersTable";
 import CompanyDefaultLayout from "../components/CompanyDefaultLayout";
-import checkCompanyToken from "../functions/auth/checkCompanyToken";
 import AddRecruiter from "../components/Recruiters/AddRecruiter";
 
 function CompanyRecruitersPage() {
-  checkCompanyToken()
   const [recruiters, setRecruiters] = useState([]);
   return (
     <CompanyDefaultLayout>
