a1822a9e86c09d4d5721c0e9133186c8871b6130
eyalyehiely <eyalwork0@gmail.com>
2024-12-04 17:40:36 +0700

    change func name

---
+++ src/Talents/functions/crud/getUserDetails.js
@@ -0,0 +1,20 @@
+import axios from '../../../generalFunctions/config/axiosConfig';
+
+export default async function getUserDetails(token, setUser, user_id) {
+  try {
+    const response = await axios.get(`/authentication/${user_id}/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status === 200) {
+      setUser(response.data);
+    } else {
+      console.log('Error:', response.data.message);
+    }
+  } catch (error) {
+    console.error('There was an error in fetching current user data!', error);
+  }
+}
