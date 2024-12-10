321b702a2c5e5d6b889586dce88177312015d002
eyalyehiely <eyalwork0@gmail.com>
2024-12-11 02:01:06 +0700

    get routes details func

---
+++ src/Talents/functions/crud/getRoutesDetails.js
@@ -0,0 +1,20 @@
+import axios from '../../../generalFunctions/config/axiosConfig';
+
+export default async function getRoutesDetails(token, setRoutes) {
+  try {
+    const response = await axios.get(`/authenticate/manage-route/`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status === 200) {
+      setRoutes(response.data.activities);
+    } else {
+      console.log('Error:', response.data.message);
+    }
+  } catch (error) {
+    console.error('There was an error in fetching current Routes data!', error);
+  }
+}
