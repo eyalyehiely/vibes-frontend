356e3f62badbfa5f0eb27f385f0fb9e9045c83c1
eyalyehiely <eyalwork0@gmail.com>
2024-12-16 16:34:23 +0700

    update command & vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allows the server to be accessible externally
-    port: parseInt(process.env.PORT) || 4173, // Uses the PORT environment variable or defaults to 4173
-  },
-  build: {
-    outDir: 'dist', // Ensure the build output goes to 'dist'
+    host: '0.0.0.0', // Ensure the app is accessible externally
+    port: parseInt(process.env.PORT) || 4173, // Use the PORT environment variable
   },
 });
