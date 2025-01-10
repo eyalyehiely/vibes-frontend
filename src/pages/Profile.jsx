25975547eb74182742bf67bc2b45ea53bcbc4660
eyalyehiely <eyalwork0@gmail.com>
2025-01-10 22:00:26 +0700

    add profile pic handling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,262 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import DefaultLayout from "../components/DefaultLayout";
-// import {
-//   Button,
-//   Card,
-//   CardContent,
-//   CardHeader,
-//   Divider,
-//   Grid,
-//   Typography,
-//   TextField,
-// } from "@mui/material";
-// import swal from "sweetalert";
-// import getUserDetails from "../utils/crud/user/getUserDetails";
-// import sendContactUsEmail from "../utils/sendContactUsEmail";
-// import UserPicHandling from "../components/User/UserPicHandling";
-// import { jwtDecode } from "jwt-decode";
-// import EditUserProfile from "../components/User/EditUserProfile";
-
-// function Profile() {
-//   const [user, setUser] = useState({});
-//   const [formData, setFormData] = useState({
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     subject: "",
-//     message: "",
-//   });
-//   const [loading, setLoading] = useState(true);
-//   const token = localStorage.getItem("authTokens");
-//   const [isDisabled, setIsDisabled] = useState(false);
-
-//   useEffect(() => {
-//     if (token) {
-//       const decodedToken = jwtDecode(token);
-//       const user_id = decodedToken.user_id;
-
-//       async function fetchUserDetails() {
-//         try {
-//           const userData = await getUserDetails(token, setUser, user_id);
-//           setFormData({
-//             ...formData,
-//             firstName: userData?.first_name || "",
-//             lastName: userData?.last_name || "",
-//             email: userData?.username || "",
-//           });
-//         } catch (error) {
-//           console.error("Error fetching user details:", error);
-//         } finally {
-//           setLoading(false);
-//         }
-//       }
-
-//       fetchUserDetails();
-//     }
-//   }, [token]);
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setIsDisabled(true);
-
-//     // Simulate form submission
-//     setTimeout(() => {
-//       setIsDisabled(false);
-//     }, 3000);
-//     if (!formData.subject.trim() || !formData.message.trim()) {
-//       swal("שגיאה", "יש למלא את שדה הנושא ואת שדה ההודעה.", "error");
-//       return;
-//     }
-
-//     sendContactUsEmail(
-//       {
-//         message: formData.message,
-//         subject: formData.subject,
-//       },
-//       token
-//     )
-//       .then(() => {
-//         setFormData({
-//           ...formData,
-//           subject: "",
-//           message: "",
-//         });
-//       })
-//       .catch((error) => {
-//         console.error("Error sending contact email:", error);
-//       });
-//   };
-
-//   if (loading) {
-//     return (
-//       <DefaultLayout>
-//         <div className="flex h-full items-center justify-center">
-//           <div className="h-10 w-10 animate-spin rounded-full border-b-2 border-blue-500"></div>
-//         </div>
-//       </DefaultLayout>
-//     );
-//   }
-
-//   return (
-//     <DefaultLayout>
-//       <div className="flex h-full p-6" dir="rtl">
-//         <div className="w-full space-y-6">
-//           {/* Account Info */}
-//           <Card
-//             sx={{
-//               boxShadow: 5,
-//               borderRadius: 3,
-//               overflow: "hidden",
-//               background: "linear-gradient(135deg, #e3f2fd, #ffffff)",
-//               border: "1px solid #90caf9",
-//             }}
-//           >
-//             <CardContent>
-//               <Grid container spacing={3} alignItems="center">
-//                 {/* Left Section: Edit Profile and User Picture */}
-//                 <Grid item xs={12} sm={3} textAlign="center">
-//                   <UserPicHandling />
-//                   <EditUserProfile setUser={setUser} user={user} />
-//                 </Grid>
-
-//                 {/* Right Section: User Details */}
-//                 <Grid item xs>
-//                   <Typography
-//                     variant="h4"
-//                     fontWeight="bold"
-//                     color="primary"
-//                     gutterBottom
-//                     sx={{ textShadow: "1px 1px 2px rgba(0, 0, 0, 0.1)" }}
-//                   >
-//                     {user.first_name} {user.last_name}
-//                   </Typography>
-//                   <Typography
-//                     variant="body1"n
-//                     color="textSecondary"
-//                     sx={{ fontStyle: "italic" }}
-//                   >
-//                     {user.username || "שם משתמש לא סופק"}
-//                   </Typography>
-//                 </Grid>
-//               </Grid>
-//             </CardContent>
-//           </Card>
-
-//           {/* Personal Details */}
-//           <Card
-//             sx={{
-//               boxShadow: 2,
-//               borderRadius: 2,
-//               overflow: "hidden",
-//             }}
-//           >
-//             <CardHeader
-//               title="פרטים אישיים"
-//               titleTypographyProps={{ variant: "h6", fontWeight: "bold" }}
-//             />
-//             <Divider />
-//             <CardContent>
-//               <Grid container spacing={3}>
-//                 <Grid item md={4} sm={6} xs={12}>
-//                   <Typography>
-//                     <strong>שם פרטי:</strong> {user.first_name || "לא סופק"}
-//                   </Typography>
-//                 </Grid>
-//                 <Grid item md={4} sm={6} xs={12}>
-//                   <Typography>
-//                     <strong>שם משפחה:</strong> {user.last_name || "לא סופק"}
-//                   </Typography>
-//                 </Grid>
-//                 <Grid item md={4} sm={6} xs={12}>
-//                   <Typography>
-//                     <strong>תאריך לידה:</strong> {user.birth_date || "לא סופק"}
-//                   </Typography>
-//                 </Grid>
-//                 <Grid item md={4} sm={6} xs={12}>
-//                   <Typography>
-//                     <strong>מגדר:</strong>{" "}
-//                     {user.gender === "זכר"
-//                       ? "זכר"
-//                       : user.gender === "נקבה"
-//                       ? "נקבה"
-//                       : "אחר"}
-//                   </Typography>
-//                 </Grid>
-//               </Grid>
-//             </CardContent>
-//           </Card>
-
-//           {/* Contact Form */}
-//           <Card
-//             sx={{
-//               boxShadow: 2,
-//               borderRadius: 2,
-//               overflow: "hidden",
-//             }}
-//           >
-//             <CardHeader
-//               title="טופס יצירת קשר"
-//               titleTypographyProps={{ variant: "h6", fontWeight: "bold" }}
-//             />
-//             <Divider />
-//             <CardContent>
-//               <form onSubmit={handleSubmit}>
-//                 <Grid container spacing={3}>
-//                   <Grid item xs={12}>
-//                     <TextField
-//                       fullWidth
-//                       label="נושא"
-//                       name="subject"
-//                       value={formData.subject}
-//                       onChange={handleChange}
-//                       variant="outlined"
-//                       required
-//                     />
-//                   </Grid>
-//                   <Grid item xs={12}>
-//                     <TextField
-//                       fullWidth
-//                       label="הודעה"
-//                       name="message"
-//                       value={formData.message}
-//                       onChange={handleChange}
-//                       variant="outlined"
-//                       multiline
-//                       rows={5}
-//                       required
-//                     />
-//                   </Grid>
-//                 </Grid>
-//                 <Button
-//                   id="submitButton"
-//                   color="primary"
-//                   type="submit"
-//                   disabled={isDisabled}
-//                   className={`w-full px-4 py-2 font-bold  ${
-//                     isDisabled ? "cursor-not-allowed opacity-50" : ""
-//                   }`}
-//                 >
-//                   {isDisabled ? "שולח הודעה..." : "שלח הודעה"}
-//                 </Button>
-//               </form>
-//             </CardContent>
-//           </Card>
-//         </div>
-//       </div>
-//     </DefaultLayout>
-//   );
-// }
-
-// export default Profile;
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import {
   Button,
@@ -267,12 +9,14 @@ import {
   Row,
   Spinner,
 } from "react-bootstrap";
+import { CiUser } from "react-icons/ci";
+import { FaCamera } from "react-icons/fa";
 import swal from "sweetalert";
 import getUserDetails from "../utils/crud/user/getUserDetails";
 import sendContactUsEmail from "../utils/sendContactUsEmail";
-import UserPicHandling from "../components/User/UserPicHandling";
 import { jwtDecode } from "jwt-decode";
 import EditUserProfile from "../components/User/EditUserProfile";
+import saveProfilePicture from "../utils/profile_picture/saveProfilePicture";
 
 function Profile() {
   const [user, setUser] = useState({});
@@ -286,6 +30,8 @@ function Profile() {
   const [loading, setLoading] = useState(true);
   const token = localStorage.getItem("authTokens");
   const [isDisabled, setIsDisabled] = useState(false);
+  const fileInputRef = useRef(null);
+
 
   useEffect(() => {
     if (token) {
@@ -362,6 +108,23 @@ function Profile() {
     );
   }
 
+  const handleFileChange = async (event) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const profilePic = {
+        file,
+        preview: URL.createObjectURL(file)
+      };
+      
+      await saveProfilePicture(profilePic, token, user_id, setUser)
+      
+    }
+  };
+
+  const triggerFileInput = () => {
+    fileInputRef.current?.click();
+  };
+
   return (
     <DefaultLayout>
       <Container className="py-4" dir="rtl">
@@ -371,7 +134,40 @@ function Profile() {
             <Row className="align-items-center">
               {/* Left Section: Edit Profile and User Picture */}
               <Col xs={12} sm={3} className="text-center">
-                <UserPicHandling />
+              <div className="relative">
+            <div className="h-40 w-40 sm:h-48 sm:w-48 md:h-52 md:w-52 rounded-full border-4 border-white shadow-md overflow-hidden bg-gray-200 -mt-24 mb-6">
+              {user?.profile_picture ? (
+                <img
+                  src={`${USERS_API_BASE_URL}${user.profile_picture}`}
+                  alt={`${user.first_name || ""} ${user.last_name || ""} Profile`}
+                  className="h-full w-full object-cover"
+                />
+              ) : (
+                <CiUser
+                  className="h-full w-full text-gray-500 p-4"
+                  aria-label="Default user icon"
+                />
+              )}
+            </div>
+
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              accept="image/*"
+              className="hidden"
+              aria-label="Upload profile picture"
+            />
+
+            <Button
+              onClick={triggerFileInput}
+              className="absolute bottom-8 right-0 rounded-full p-2 bg-white shadow-md hover:bg-gray-100"
+              aria-label="Change profile picture"
+            >
+             <FaCamera size={20} color={blue}/>
+
+            </Button>
+          </div>
                 <EditUserProfile setUser={setUser} user={user} />
               </Col>
 
