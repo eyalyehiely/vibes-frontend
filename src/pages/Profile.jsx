9c9211531e417853b4364b6ee891844c8a39abf9
eyalyehiely <eyalwork0@gmail.com>
2025-01-10 22:51:17 +0700

    update pic handling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,281 @@
+// import React, { useState, useEffect, useRef } from "react";
+// import DefaultLayout from "../components/DefaultLayout";
+// import {
+//   Button,
+//   Card,
+//   Col,
+//   Container,
+//   Form,
+//   Row,
+//   Spinner,
+// } from "react-bootstrap";
+// import { CiUser } from "react-icons/ci";
+// import { FaCamera } from "react-icons/fa";
+// import swal from "sweetalert";
+// import getUserDetails from "../utils/crud/user/getUserDetails";
+// import sendContactUsEmail from "../utils/sendContactUsEmail";
+// import { jwtDecode } from "jwt-decode";
+// import EditUserProfile from "../components/User/EditUserProfile";
+// import saveProfilePicture from "../utils/profile_picture/saveProfilePicture";
+
+// function Profile() {
+//   const [user, setUser] = useState({});
+//   const [formData, setFormData] = useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     subject: "",
+//     message: "",
+//   });
+//   const [loading, setLoading] = useState(true);
+//   const token = localStorage.getItem("authTokens");
+//   const [isDisabled, setIsDisabled] = useState(false);
+//   const fileInputRef = useRef(null);
+//   const BACKEND_API_BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL || "http://localhost:8000/api/v1/authenticate "
+
+
+//   useEffect(() => {
+//     if (token) {
+//       const decodedToken = jwtDecode(token);
+//       const user_id = decodedToken.user_id;
+
+//       async function fetchUserDetails() {
+//         try {
+//           const userData = await getUserDetails(token, setUser, user_id);
+//           setFormData({
+//             ...formData,
+//             firstName: userData?.first_name || "",
+//             lastName: userData?.last_name || "",
+//             email: userData?.username || "",
+//           });
+//         } catch (error) {
+//           console.error("Error fetching user details:", error);
+//         } finally {
+//           setLoading(false);
+//         }
+//       }
+
+//       fetchUserDetails();
+//     }
+//   }, [token]);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setIsDisabled(true);
+
+//     // Simulate form submission
+//     setTimeout(() => {
+//       setIsDisabled(false);
+//     }, 3000);
+//     if (!formData.subject.trim() || !formData.message.trim()) {
+//       swal("שגיאה", "יש למלא את שדה הנושא ואת שדה ההודעה.", "error");
+//       return;
+//     }
+
+//     sendContactUsEmail(
+//       {
+//         message: formData.message,
+//         subject: formData.subject,
+//       },
+//       token
+//     )
+//       .then(() => {
+//         setFormData({
+//           ...formData,
+//           subject: "",
+//           message: "",
+//         });
+//       })
+//       .catch((error) => {
+//         console.error("Error sending contact email:", error);
+//       });
+//   };
+
+//   if (loading) {
+//     return (
+//       <DefaultLayout>
+//         <div className="d-flex justify-content-center align-items-center h-100">
+//           <Spinner animation="border" variant="primary" />
+//         </div>
+//       </DefaultLayout>
+//     );
+//   }
+
+//   const handleFileChange = async (event) => {
+//     const file = event.target.files?.[0];
+//     if (file) {
+//       const maxFileSize = 5 * 1024 * 1024; // 5 MB
+//       if (!file.type.startsWith("image/")) {
+//         swal("Error", "Only image files are allowed", "error");
+//         return;
+//       }
+//       if (file.size > maxFileSize) {
+//         swal("Error", "File size exceeds 5MB", "error");
+//         return;
+//       }
+
+//       const profilePic = {
+//         file,
+//         preview: URL.createObjectURL(file)
+//       };
+
+//       await saveProfilePicture(profilePic, token, user.id, setUser);
+//     }
+//   };
+//   const triggerFileInput = () => {
+//     fileInputRef.current?.click();
+//   };
+
+//   return (
+//     <DefaultLayout>
+//       <Container className="py-4" dir="rtl">
+//         {/* Account Info */}
+//         <Card className="mb-4 shadow-sm">
+//           <Card.Body>
+//             <Row className="align-items-center">
+//               {/* Left Section: Edit Profile and User Picture */}
+//               <Col xs={12} sm={3} className="text-center">
+//                 <div className="relative">
+//                   <div className="h-40 w-40 sm:h-48 sm:w-48 md:h-52 md:w-52 rounded-full border-4 border-white shadow-md overflow-hidden bg-gray-200 -mt-24 mb-6">
+//                     {user?.profile_picture ? (
+//                       <img
+//                         src={user.profile_picture}
+//                         alt={`${user.first_name || ""} ${user.last_name || ""} Profile`}
+//                         className="h-full w-full object-cover"
+//                       />
+//                     ) : (
+//                       <CiUser
+//                         className="h-full w-full text-gray-500 p-4"
+//                         aria-label="Default user icon"
+//                       />
+//                     )}
+//                   </div>
+
+//                   <input
+//                     type="file"
+//                     ref={fileInputRef}
+//                     onChange={handleFileChange}
+//                     accept="image/*"
+//                     className="hidden"
+//                     aria-label="Upload profile picture"
+//                   />
+
+//                   <Button
+//                     onClick={triggerFileInput}
+//                     className="absolute bottom-8 right-0 rounded-full p-2 bg-white shadow-md hover:bg-gray-100"
+//                     aria-label="Change profile picture"
+//                   >
+//                     <FaCamera size={20} />
+//                   </Button>
+//                 </div>
+//                 <EditUserProfile setUser={setUser} user={user} />
+//               </Col>
+
+//               {/* Right Section: User Details */}
+//               <Col xs>
+//                 <h4 className="fw-bold text-primary mb-3">
+//                   {user.first_name} {user.last_name}
+//                 </h4>
+//                 <p className="fst-italic text-muted">
+//                   {user.username || "שם משתמש לא סופק"}
+//                 </p>
+//               </Col>
+//             </Row>
+//           </Card.Body>
+//         </Card>
+
+//         {/* Personal Details */}
+//         <Card className="mb-4 shadow-sm">
+//           <Card.Header as="h6" className="fw-bold">
+//             פרטים אישיים
+//           </Card.Header>
+//           <Card.Body>
+//             <Row>
+//               <Col md={4} sm={6} xs={12}>
+//                 <strong>שם פרטי:</strong> {user.first_name || "לא סופק"}
+//               </Col>
+//               <Col md={4} sm={6} xs={12}>
+//                 <strong>שם משפחה:</strong> {user.last_name || "לא סופק"}
+//               </Col>
+//               <Col md={4} sm={6} xs={12}>
+//                 <strong>תאריך לידה:</strong> {user.birth_date || "לא סופק"}
+//               </Col>
+//               <Col md={4} sm={6} xs={12}>
+//                 <strong>מגדר:</strong>{" "}
+//                 {user.gender === "זכר"
+//                   ? "זכר"
+//                   : user.gender === "נקבה"
+//                     ? "נקבה"
+//                     : "אחר"}
+//               </Col>
+//             </Row>
+//           </Card.Body>
+//         </Card>
+
+//         {/* Contact Form */}
+//         <Card className="shadow-sm">
+//           <Card.Header as="h6" className="fw-bold">
+//             טופס יצירת קשר
+//           </Card.Header>
+//           <Card.Body>
+//             <Form onSubmit={handleSubmit}>
+//               <Row className="mb-3">
+//                 <Col>
+//                   <Form.Group controlId="formSubject">
+//                     <Form.Label>נושא</Form.Label>
+//                     <Form.Control
+//                       type="text"
+//                       name="subject"
+//                       value={formData.subject}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </Form.Group>
+//                 </Col>
+//               </Row>
+//               <Row className="mb-3">
+//                 <Col>
+//                   <Form.Group controlId="formMessage">
+//                     <Form.Label>הודעה</Form.Label>
+//                     <Form.Control
+//                       as="textarea"
+//                       rows={5}
+//                       name="message"
+//                       value={formData.message}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </Form.Group>
+//                 </Col>
+//               </Row>
+//               <Button
+//                 type="submit"
+//                 variant="primary"
+//                 disabled={isDisabled}
+//                 className={`w-100 fw-bold ${isDisabled ? "disabled opacity-50" : ""
+//                   }`}
+//               >
+//                 {isDisabled ? "שולח הודעה..." : "שלח הודעה"}
+//               </Button>
+//             </Form>
+//           </Card.Body>
+//         </Card>
+//       </Container>
+//     </DefaultLayout>
+//   );
+// }
+
+// export default Profile;
+// Profile.jsx
+
 import React, { useState, useEffect, useRef } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import {
@@ -14,7 +292,7 @@ import { FaCamera } from "react-icons/fa";
 import swal from "sweetalert";
 import getUserDetails from "../utils/crud/user/getUserDetails";
 import sendContactUsEmail from "../utils/sendContactUsEmail";
-import { jwtDecode } from "jwt-decode";
+import {jwtDecode} from "jwt-decode"; // Corrected import
 import EditUserProfile from "../components/User/EditUserProfile";
 import saveProfilePicture from "../utils/profile_picture/saveProfilePicture";
 
@@ -31,25 +309,25 @@ function Profile() {
   const token = localStorage.getItem("authTokens");
   const [isDisabled, setIsDisabled] = useState(false);
   const fileInputRef = useRef(null);
-  const BACKEND_API_BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL || "http://localhost:8000/api/v1/authenticate "
-
+  const BACKEND_API_BASE_URL = import.meta.env.VITE_BACKEND_API_BASE_URL || "http://localhost:8000/api/v1/authenticate";
 
   useEffect(() => {
     if (token) {
       const decodedToken = jwtDecode(token);
-      const user_id = decodedToken.user_id;
+      // const user_id = decodedToken.user_id; // Removed
 
       async function fetchUserDetails() {
         try {
-          const userData = await getUserDetails(token, setUser, user_id);
-          setFormData({
-            ...formData,
+          const userData = await getUserDetails(token, setUser);
+          setFormData(prevFormData => ({
+            ...prevFormData,
             firstName: userData?.first_name || "",
             lastName: userData?.last_name || "",
             email: userData?.username || "",
-          });
+          }));
         } catch (error) {
           console.error("Error fetching user details:", error);
+          swal("Error", "Failed to fetch user details.", "error");
         } finally {
           setLoading(false);
         }
@@ -61,10 +339,10 @@ function Profile() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
+    setFormData(prevFormData => ({
+      ...prevFormData,
       [name]: value,
-    });
+    }));
   };
 
   const handleSubmit = (e) => {
@@ -88,14 +366,16 @@ function Profile() {
       token
     )
       .then(() => {
-        setFormData({
-          ...formData,
+        setFormData(prevFormData => ({
+          ...prevFormData,
           subject: "",
           message: "",
-        });
+        }));
+        swal("Success", "Your message has been sent!", "success");
       })
       .catch((error) => {
         console.error("Error sending contact email:", error);
+        swal("Error!", "Failed to send message.", "error");
       });
   };
 
@@ -124,12 +404,13 @@ function Profile() {
 
       const profilePic = {
         file,
-        preview: URL.createObjectURL(file)
+        preview: URL.createObjectURL(file),
       };
 
-      await saveProfilePicture(profilePic, token, user.id, setUser);
+      await saveProfilePicture(profilePic, token, setUser);
     }
   };
+
   const triggerFileInput = () => {
     fileInputRef.current?.click();
   };
@@ -145,7 +426,7 @@ function Profile() {
               <Col xs={12} sm={3} className="text-center">
                 <div className="relative">
                   <div className="h-40 w-40 sm:h-48 sm:w-48 md:h-52 md:w-52 rounded-full border-4 border-white shadow-md overflow-hidden bg-gray-200 -mt-24 mb-6">
-                    {user?.profile_picture ? (
+                    {user.profile_picture ? (
                       <img
                         src={user.profile_picture}
                         alt={`${user.first_name || ""} ${user.last_name || ""} Profile`}
@@ -260,8 +541,9 @@ function Profile() {
                 type="submit"
                 variant="primary"
                 disabled={isDisabled}
-                className={`w-100 fw-bold ${isDisabled ? "disabled opacity-50" : ""
-                  }`}
+                className={`w-100 fw-bold ${
+                  isDisabled ? "disabled opacity-50" : ""
+                }`}
               >
                 {isDisabled ? "שולח הודעה..." : "שלח הודעה"}
               </Button>
--- src/utils/profile_picture/saveProfilePicture.js
+++ src/utils/profile_picture/saveProfilePicture.js
@@ -1,14 +1,16 @@
+// saveProfilePicture.js
+
 import axios from '../../utils/config/axiosConfig';
 import swal from 'sweetalert';
 import getUserDetails from "../crud/user/getUserDetails";
 
-export default async function saveProfilePicture(profilePic, token, user_id, setUser) {
+export default async function saveProfilePicture(profilePic, token, setUser) {
   if (profilePic && profilePic.file) {
     const formData = new FormData();
     formData.append('profile_picture', profilePic.file);
 
     try {
-      const response = await axios.post(`/authenticate/manage-profile-pic/${user_id}/`, formData, {
+      const response = await axios.post(`/authenticate/manage-profile-pic/`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${token}`,
@@ -22,8 +24,6 @@ export default async function saveProfilePicture(profilePic, token, user_id, set
           timer: 1000,
           button: false,
         });
-        // Re-fetch the user details after saving the profile picture
-        getUserDetails(token, setUser, user_id);
       }
     } catch (error) {
       console.error('Error saving profile picture:', error);
